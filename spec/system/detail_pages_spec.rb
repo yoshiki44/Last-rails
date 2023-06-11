02e68825a8f01166600433ac788bda34216edca2
yoshiki44 <ys44oct@gmail.com>
2023-06-11 13:25:06 +0000

    system specの実装、title変更(rubocopにて修正)

--- spec/system/detail_pages_spec.rb
+++ spec/system/detail_pages_spec.rb
@@ -5,23 +5,21 @@ RSpec.describe "詳細ページ", type: :system do
   let!(:product) { create(:product) }
   let!(:image) { create(:image) }
 
-  it"詳細ページのロゴからトップページへ遷移できること" do
-      visit potepan_product_path(product.id)
-      find(".navbar-link").click
-      expect(current_path).to eq(potepan_index_path)
+  it "詳細ページのロゴからトップページへ遷移できること" do
+    visit potepan_product_path(product.id)
+    find(".navbar-link").click
+    expect(current_path).to eq(potepan_index_path)
   end
 
-  it"詳細ページのヘッダーのHOMEからトップページへ遷移できること" do
-      visit potepan_product_path(product.id)
-      find(".header-home").click
-      expect(current_path).to eq(potepan_index_path)
+  it "詳細ページのヘッダーのHOMEからトップページへ遷移できること" do
+    visit potepan_product_path(product.id)
+    find(".header-home").click
+    expect(current_path).to eq(potepan_index_path)
   end
 
-  it"詳細ページのページヘッダーのHOMEからトップページへ遷移できること" do
-      visit potepan_product_path(product.id)
-      find(".pageheader-home").click
-      expect(current_path).to eq(potepan_index_path)
+  it "詳細ページのページヘッダーのHOMEからトップページへ遷移できること" do
+    visit potepan_product_path(product.id)
+    find(".pageheader-home").click
+    expect(current_path).to eq(potepan_index_path)
   end
-
-
 end
