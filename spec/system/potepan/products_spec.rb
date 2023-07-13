666eeb68f3748791a42a68346fdb9ce2b4759da2
yoshiki44 <ys44oct@gmail.com>
2023-07-13 12:58:07 +0000

    products_specの変数の名称変更

--- spec/system/potepan/products_spec.rb
+++ spec/system/potepan/products_spec.rb
@@ -2,16 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Products', type: :system do
   let(:taxonomy) { create(:taxonomy) }
-  let(:taxon1) { create(:taxon, taxonomy: taxonomy) }
-  let(:related_product) { create(:product, taxons: [taxon1]) }
-  let(:unrelated_product) { create(:product) }
+  let(:taxon) { create(:taxon, taxonomy: taxonomy) }
+  let(:product) { create(:product, taxons: [taxon]) }
   let(:related_image) { create(:image) }
-  let(:unrelated_image) { create(:image) }
 
   before do
-    related_product.images << related_image
-    unrelated_product.images << unrelated_image
-    visit potepan_product_path(related_product.id)
+    product.images << related_image
+    visit potepan_product_path(product.id)
   end
 
   it '詳細ページのロゴからトップページへ遷移できること' do
@@ -37,18 +34,18 @@ RSpec.describe 'Products', type: :system do
 
   it '一覧ページへ戻れること' do
     click_on '一覧ページへ戻る'
-    expect(current_path).to eq potepan_category_path(related_product.taxons.first.id)
+    expect(current_path).to eq potepan_category_path(product.taxons.first.id)
   end
 
   it '商品名を表示すること' do
-    expect(page).to have_content related_product.name
+    expect(page).to have_content product.name
   end
 
   it '商品単価を表示すること' do
-    expect(page).to have_content related_product.display_price.to_s
+    expect(page).to have_content product.display_price.to_s
   end
 
   it '商品説明を表示すること' do
-    expect(page).to have_content related_product.description
+    expect(page).to have_content product.description
   end
 end
