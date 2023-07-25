2c52fa2fb34bb45ef601af19838af80d0e0d7762
yoshiki44 <ys44oct@gmail.com>
2023-07-25 14:02:15 +0000

    関連商品の表示数を定数で定義

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,7 +1,9 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_RELATED_PRODUCTS_NUMBER = 4
+
   def show
     @product = Spree::Product.find(params[:id])
     @taxons = Spree::Taxon.find(@product.taxons.ids)
-    @related_products = @product.related_products.includes(master: %i[images default_price]).limit(4)
+    @related_products = @product.related_products.includes(master: %i[images default_price]).limit(MAX_RELATED_PRODUCTS_NUMBER)
   end
 end
